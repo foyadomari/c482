d41d06812e517eeda1aad9765b01d8ccb3fe7a2f
foyadomari <45193676+foyadomari@users.noreply.github.com>
2022-11-19 09:49:26 -0800

    added the required part.java and refactored the main form fxml

---
+++ src/main/java/model/Inventory.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Inventory {
+
+    /**
+     *
+     * @param newPart
+     */
+    public static void addPart(Part newPart){
+
+    }
+
+    public static void addProduct(Product newProduct){
+
+    }
+
+}
